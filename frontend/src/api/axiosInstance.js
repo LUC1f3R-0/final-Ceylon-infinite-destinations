41a28e978317c199b0b09823ace090d451af813d
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-05-20 17:55:34 +0530

    for check

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // console.log("back end server", import.meta.env.VITE_BACKED_URL)
 const axiosInstance = axios.create({
-    baseURL: "https://5dcf-212-104-231-69.ngrok-free.app",
+    baseURL: "https://1460-212-104-231-69.ngrok-free.app",
     timeout: 5000,
     headers: { 'Content-Type': 'application/json' }
 })
