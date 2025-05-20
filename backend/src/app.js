30150564c51d59d8b6e11517757cb9efc5b9dd5e
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-05-21 00:08:55 +0530

    secured

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,16 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import destinationRoute from './routes/destinations.route.js';
+import apiKeyMiddleware from './Middlewares/apiKey.middleware.js';
 
 const app = express();
 
 app.use(express.json())
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
-app.use('/destination', destinationRoute)
+const allowedOrigin = process.env.FRONTEND_URL;
+
+app.use(cors({ origin: function (origin, callback) { if (!origin || origin === allowedOrigin) { callback(null, true); } else { callback(new Error('Not allowed by CORS')); } }, credentials: true, }));
+
+app.use('/destination', apiKeyMiddleware, destinationRoute)
 
 export default app;
