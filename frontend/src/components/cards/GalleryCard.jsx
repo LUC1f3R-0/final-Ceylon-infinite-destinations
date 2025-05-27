a9bb97a2161d04fefe73b9d69e76ecf4058a1bea
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-05-27 19:44:34 +0530

    updated

--- frontend/src/components/cards/GalleryCard.jsx
+++ frontend/src/components/cards/GalleryCard.jsx
@@ -1,30 +1,18 @@
 import React from 'react'
 
-const GalleryCard = (img) => {
-    console.log(img)
+const GalleryCard = ({ id, name, defltPath, hovrPath }) => {
+    console.log(id, name, defltPath, hovrPath)
     return (
         <>
-            <div
-                key={img.id}
-                className="relative overflow-hidden rounded-lg shadow-lg cursor-pointer group"
-            >
-                {/* Default Image */}
+            <div key={id} className="relative overflow-hidden rounded-lg shadow-lg cursor-pointer group">
                 <img
-                    src={img.defltPath}
-                    alt={img.name}
+                    src={defltPath}
+                    alt={name}
                     className="w-full h-64 object-cover transition-opacity duration-500 group-hover:opacity-0"
                 />
-
-                {/* Hover Image */}
-                <img
-                    src={img.hovrPath}
-                    alt={`${img.name} - Hover`}
-                    className="absolute inset-0 w-full h-64 object-cover opacity-0 transition-opacity duration-500 group-hover:opacity-100"
-                />
-
-                {/* Image Name Overlay */}
+                <img src={hovrPath} alt={`${name} - Hover`} className="absolute inset-0 w-full h-64 object-cover opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
                 <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-                    {img.name}
+                    {name}
                 </div>
             </div>
         </>
